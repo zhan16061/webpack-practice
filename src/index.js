67060f2b62f073f3cbb84836a1a5fabdf2cdc992
Nick <zhan16061@gmail.com>
2020-02-23 20:01:21 +0800

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
+  btn.innerHTML = 'click me'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
   return element
 }
 

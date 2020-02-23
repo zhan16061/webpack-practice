bdb799265950527668cf8d3697ffeb17bc7c3ea6
Nick <zhan16061@gmail.com>
2020-02-23 18:39:50 +0800

    add css、file、style loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        // .png|svg|jpg|gif $: 結尾
+        test:/\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ],
+  },
 }
